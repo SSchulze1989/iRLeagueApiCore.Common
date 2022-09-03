a03194438227f2075bb767f110e06a1c5bb9f828
Simon Schulze <simonschulze2589@hotmail.de>
2022-09-03 11:40:19 +0200

    Add resultconfig models

---
+++ src/iRLeagueApiCore.Common/Models/Results/PostResultConfigModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueApiCore.Common.Models
+{
+    [DataContract]
+    public class PostResultConfigModel
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string DisplayName { get; set; }
+        [DataMember]
+        public IEnumerable<ScoringModel> Scorings { get; set; }
+    }
+}
