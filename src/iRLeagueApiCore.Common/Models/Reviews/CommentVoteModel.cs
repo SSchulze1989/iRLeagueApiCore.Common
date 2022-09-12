8b71ab880d3113222a3efaba620f4f3a52be4c00
Simon Schulze <simonschulze2589@hotmail.de>
2022-09-12 07:56:33 +0200

    Add missing fields

--- src/iRLeagueApiCore.Common/Models/Reviews/CommentVoteModel.cs
+++ src/iRLeagueApiCore.Common/Models/Reviews/CommentVoteModel.cs
@@ -6,8 +6,13 @@ namespace iRLeagueApiCore.Common.Models.Reviews
     [DataContract]
     public class CommentVoteModel
     {
+        [DataMember]
         public long Id { get; set; }
+        [DataMember]
+        public long VoteCategoryId { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
         public MemberInfoModel MemberAtFault { get; set; }
-        public VoteCategoryModel VoteCategoryId { get; set; }
     }
 }
