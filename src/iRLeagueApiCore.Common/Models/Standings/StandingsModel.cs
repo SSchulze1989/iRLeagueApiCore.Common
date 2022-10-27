4b36797ee3ad61e3f9558a9b3511fd6b2bef5599
Simon Schulze <simonschulze2589@hotmail.de>
2022-10-27 18:36:00 +0200

    Add standing models

---
+++ src/iRLeagueApiCore.Common/Models/Standings/StandingsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iRLeagueApiCore.Common.Models.Standings
+{
+    public class StandingsModel
+    {
+        public long StandingId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public IEnumerable<StandingRowModel> StandingRows { get; set; } = Array.Empty<StandingRowModel>();
+    }
+}
