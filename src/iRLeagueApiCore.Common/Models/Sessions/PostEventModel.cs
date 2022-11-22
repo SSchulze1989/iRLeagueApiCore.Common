624c6be148d2375894ec7e32c5814e5af2782cad
Simon Schulze <simonschulze2589@hotmail.de>
2022-11-23 00:17:46 +0100

    Add result config info to event

--- src/iRLeagueApiCore.Common/Models/Sessions/PostEventModel.cs
+++ src/iRLeagueApiCore.Common/Models/Sessions/PostEventModel.cs
@@ -1,39 +1,33 @@
-﻿using iRLeagueApiCore.Common.Enums;
-using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
+﻿namespace iRLeagueApiCore.Common.Models;
 
-namespace iRLeagueApiCore.Common.Models
+[DataContract]
+public class PostEventModel
 {
-    [DataContract]
-    public class PostEventModel
-    {
-        public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
-        [DataMember]
-#if NETCOREAPP
-        [System.ComponentModel.DataAnnotations.EnumDataType(typeof(EventType))]
-#endif
-        public EventType EventType { get; set; }
-        /// <summary>
-        /// Day and time of session start
-        /// </summary>
-        [DataMember]
-        public DateTime? Date { get; set; }
-        /// <summary>
-        /// Track id of the location
-        /// </summary>
-        [DataMember]
-        public long? TrackId { get; set; }
-        /// <summary>
-        /// Total duration of the session including all Subsessions and events
-        /// </summary>
-        [DataMember]
-        public TimeSpan Duration { get; set; }
-        /// <summary>
-        /// Sessions configured for this event
-        /// </summary>
-        [DataMember]
-        public ICollection<SessionModel> Sessions { get; set; } = new List<SessionModel>();
-    }
+    [DataMember]
+    [EnumDataType(typeof(EventType))]
+    public EventType EventType { get; set; }
+    /// <summary>
+    /// Day and time of session start
+    /// </summary>
+    [DataMember]
+    public DateTime? Date { get; set; }
+    /// <summary>
+    /// Track id of the location
+    /// </summary>
+    [DataMember]
+    public long? TrackId { get; set; }
+    /// <summary>
+    /// Total duration of the session including all Subsessions and events
+    /// </summary>
+    [DataMember]
+    public TimeSpan Duration { get; set; }
+    /// <summary>
+    /// Sessions configured for this event
+    /// </summary>
+    [DataMember]
+    public ICollection<SessionModel> Sessions { get; set; } = new List<SessionModel>();
+    [DataMember]
+    public ICollection<ResultConfigInfoModel> ResultConfigs { get; set; } = new List<ResultConfigInfoModel>();
 }
