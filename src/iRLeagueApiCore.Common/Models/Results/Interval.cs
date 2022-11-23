c4be38c510ae47c30a4b17b5a65749ffb4a1533c
Simon Schulze <simonschulze2589@hotmail.de>
2022-11-23 10:52:51 +0100

    Add interval type

---
+++ src/iRLeagueApiCore.Common/Models/Results/Interval.cs
@@ -0,0 +1,10 @@
+﻿namespace iRLeagueApiCore.Common.Models;
+
+[DataContract]
+public struct Interval
+{
+    [DataMember]
+    public TimeSpan Time { get; set; }
+    [DataMember]
+    public int Laps { get; set; }
+}
