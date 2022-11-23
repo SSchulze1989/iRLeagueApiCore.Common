34a53d3610b71843e8b1b14e0d6f45cb33fea322
Simon Schulze <simonschulze2589@hotmail.de>
2022-11-23 12:05:49 +0100

    Implement IComparable on Interval

--- src/iRLeagueApiCore.Common/Models/Results/Interval.cs
+++ src/iRLeagueApiCore.Common/Models/Results/Interval.cs
@@ -1,7 +1,7 @@
 ﻿namespace iRLeagueApiCore.Common.Models;
 
 [DataContract]
-public struct Interval
+public struct Interval : IComparable, IComparable<Interval>
 {
     public Interval()
     {
@@ -19,4 +19,18 @@ public struct Interval
     public TimeSpan Time { get; set; }
     [DataMember]
     public int Laps { get; set; }
+
+    int IComparable<Interval>.CompareTo(Interval other)
+    {
+        return ((IComparable)this).CompareTo(other);
+    }
+
+    int IComparable.CompareTo(object? obj)
+    {
+        if (obj is Interval other)
+        {
+            return Time.Add(TimeSpan.FromDays(Laps)).CompareTo(other.Time.Add(TimeSpan.FromDays(other.Laps)));
+        }
+        return 1;
+    }
 }
