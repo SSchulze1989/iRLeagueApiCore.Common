ce90b8c364e0d993a418fd16db73786c0276f49a
Simon Schulze <simonschulze2589@hotmail.de>
2022-11-24 01:14:09 +0100

    Update filter model

--- src/iRLeagueApiCore.Common/Models/ResultConfigurations/ResultFilterModel.cs
+++ src/iRLeagueApiCore.Common/Models/ResultConfigurations/ResultFilterModel.cs
@@ -4,17 +4,22 @@
 public sealed class ResultFilterModel
 {
     [DataMember]
-    public long ResultsFilterId { get; set; }
-    [DataMember]
     public long LeagueId { get; set; }
     [DataMember]
-    public string ResultsFilterType { get; set; } = string.Empty;
+    public long FilterOptionId { get; set; }
+    [DataMember]
+    public FilterType FilterType { get; set; }
     [DataMember]
     public string ColumnPropertyName { get; set; } = string.Empty;
     [DataMember]
     public ComparatorType Comparator { get; set; }
+    /// <summary>
+    /// <para>Action performed on values that match the conditions</para>   
+    /// <para>- Keep: Keep the entries that match and remove all other entries</para>
+    /// <para>- Remove: Remove the entries that match and keep other entries</para>
+    /// </summary>
     [DataMember]
-    public bool Include { get; set; }
+    public MatchedValueAction Action { get; set; }
     [DataMember]
     public ICollection<string> FilterValues { get; set; } = Array.Empty<string>();
 }
