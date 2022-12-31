60f2e397abce4c5db93a4f2bd7269ab60db78fe4
Simon Schulze <simonschulze2589@hotmail.de>
2022-12-31 14:25:58 +0100

    Add protest models

---
+++ src/iRLeagueApiCore.Common/Models/Reviews/PostProtestModel.cs
@@ -0,0 +1,24 @@
+﻿namespace iRLeagueApiCore.Common.Models;
+
+[DataContract]
+public class PostProtestModel
+{
+    [DataMember]
+    public long ProtestId { get; set; }
+    [DataMember]
+    public string AuthorName { get; set; } = string.Empty;
+    [DataMember]
+    public string ConfirmIRacingId { get; set; } = string.Empty;
+    [DataMember]
+    public long EventId { get; set; }
+    [DataMember]
+    public int SessionNr { get; set; }
+    [DataMember]
+    public string FullDescription { get; set; } = string.Empty;
+    [DataMember]
+    public string OnLap { get; set; } = string.Empty;
+    [DataMember]
+    public string Corner { get; set; } = string.Empty;
+    [DataMember]
+    public ICollection<long> InvolvedMemberIds { get; set; } = new List<long>();
+}
