8d511d89ba86272b11fecca2669e1ed6ef592b60
Simon Schulze <simonschulze2589@hotmail.de>
2023-02-06 16:49:08 +0100

    Add championship and season name to champseason

--- src/iRLeagueApiCore.Common/Models/Championships/ChampSeasonModel.cs
+++ src/iRLeagueApiCore.Common/Models/Championships/ChampSeasonModel.cs
@@ -9,4 +9,8 @@ public sealed class ChampSeasonModel : PutChampSeasonModel
     public long ChampionshipId { get; set; }
     [DataMember]
     public long SeasonId { get; set; }
+    [DataMember]
+    public string ChampionshipName { get; set; } = string.Empty;
+    [DataMember]
+    public string SeasonName { get; set; } = string.Empty;
 }
