418d3a1e9737eabca39416704d3c93b7d0479907
Simon Schulze <simonschulze2589@hotmail.de>
2023-02-18 13:59:41 +0100

    Post empty champ season to handle creation of instances

--- src/iRLeagueApiCore.Common/Models/Championships/PostChampSeasonModel.cs
+++ src/iRLeagueApiCore.Common/Models/Championships/PostChampSeasonModel.cs
@@ -3,10 +3,4 @@
 [DataContract]
 public class PostChampSeasonModel
 {
-    [DataMember]
-    public bool IsActive { get; set; }
-    [DataMember]
-    public StandingConfigModel? StandingConfig { get; set; }
-    [DataMember]
-    public ICollection<ResultConfigInfoModel> ResultConfigs { get; set; } = new List<ResultConfigInfoModel>();
 }
--- src/iRLeagueApiCore.Common/Models/Championships/PutChampSeasonModel.cs
+++ src/iRLeagueApiCore.Common/Models/Championships/PutChampSeasonModel.cs
@@ -3,4 +3,10 @@
 [DataContract]
 public class PutChampSeasonModel : PostChampSeasonModel
 {
+    [DataMember]
+    public bool IsActive { get; set; }
+    [DataMember]
+    public StandingConfigModel? StandingConfig { get; set; }
+    [DataMember]
+    public ICollection<ResultConfigInfoModel> ResultConfigs { get; set; } = new List<ResultConfigInfoModel>();
 }
