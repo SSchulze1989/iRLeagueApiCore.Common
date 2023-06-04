9c3ba7917bf4587c3695e0f5259aba24f5c7bd31
Simon Schulze <simonschulze2589@hotmail.de>
2023-06-04 13:12:23 +0200

    Update penalty model

--- src/iRLeagueApiCore.Common/Models/Reviews/PenaltyModel.cs
+++ src/iRLeagueApiCore.Common/Models/Reviews/PenaltyModel.cs
@@ -1,14 +1,16 @@
-﻿namespace iRLeagueApiCore.Common.Models.Reviews;
+﻿namespace iRLeagueApiCore.Common.Models;
 
 [DataContract]
-public sealed class ReviewPenaltyModel
+public sealed class PenaltyModel
 {
     [DataMember]
     public long ResultRowId { get; set; }
     [DataMember]
-    public long ReviewId { get; set; }
-    [DataMember]
-    public long ReviewVoteId { get; set; }
+    public long? AddPenaltyId { get; set; }
+    [DataMember(EmitDefaultValue = false)]
+    public long? ReviewId { get; set; }
+    [DataMember(EmitDefaultValue = false)]
+    public long? ReviewVoteId { get; set; }
     [DataMember]
     public PenaltyType Type { get; set; }
     [DataMember(EmitDefaultValue = false)]
