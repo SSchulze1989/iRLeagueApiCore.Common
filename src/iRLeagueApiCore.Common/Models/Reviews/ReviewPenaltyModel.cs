5612bd0eb485b04fbae0449c4b1ccdc96a913c9f
Simon Schulze <simonschulze2589@hotmail.de>
2023-06-04 02:54:25 +0200

    add review penalty model

---
+++ src/iRLeagueApiCore.Common/Models/Reviews/ReviewPenaltyModel.cs
@@ -0,0 +1,20 @@
+﻿namespace iRLeagueApiCore.Common.Models.Reviews;
+
+[DataContract]
+public sealed class ReviewPenaltyModel
+{
+    [DataMember]
+    public long ResultRowId { get; set; }
+    [DataMember]
+    public long ReviewId { get; set; }
+    [DataMember]
+    public long ReviewVoteId { get; set; }
+    [DataMember]
+    public PenaltyType Type { get; set; }
+    [DataMember(EmitDefaultValue = false)]
+    public int Points { get; set; }
+    [DataMember(EmitDefaultValue = false)]
+    public TimeSpan Time { get; set; }
+    [DataMember(EmitDefaultValue = false)]
+    public int Positions { get; set; }
+}
