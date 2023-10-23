a2c738973643bb3aedb95e9cae06ff0fb0d18025
Simon Schulze <simonschulze2589@hotmail.de>
2023-10-23 22:09:48 +0200

    Add info model for cars in event

---
+++ src/iRLeagueApiCore.Common/Models/Results/EventCarInfoModel.cs
@@ -0,0 +1,23 @@
+﻿namespace iRLeagueApiCore.Common.Models.Results;
+
+/// <summary>
+/// Contains information about a specific car in an event and it's driver(s) or team
+/// </summary>
+[DataContract]
+public sealed class EventCarInfoModel
+{
+    [DataMember]
+    public int Number { get; set; }
+    [DataMember] 
+    public string Car { get; set; } = string.Empty;
+    [DataMember]
+    public int CarId { get; set; }
+    [DataMember] 
+    public int ClassId { get;}
+    [DataMember]
+    public string Class { get; set; } = string.Empty;
+    [DataMember]
+    public MemberInfoModel? Member { get; set; }
+    [DataMember]
+    public TeamInfoModel? Team { get; set; }
+}
