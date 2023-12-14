05ba4928927b218288cfe8f287c68b1202e720aa
Simon Schulze <simonschulze2589@hotmail.de>
2023-12-14 09:42:28 +0100

    Add more metadata to AddBonus

--- src/iRLeagueApiCore.Common/Models/Results/AddBonusModel.cs
+++ src/iRLeagueApiCore.Common/Models/Results/AddBonusModel.cs
@@ -1,5 +1,20 @@
 ﻿namespace iRLeagueApiCore.Common.Models.Results;
+
+[DataContract]
 public sealed class AddBonusModel : PutAddBonusModel
 {
+    [DataMember]
     public long AddBonusId { get; set; }
+    [DataMember]
+    public long ResultRowId { get; set; }
+    [DataMember]
+    public long EventId { get; set; }
+    [DataMember]
+    public int SessionNr { get; set; }
+    [DataMember]
+    public string SessionName { get; set; } = string.Empty;
+    [DataMember]
+    public MemberInfoModel? Member { get; set; }
+    [DataMember]
+    public TeamInfoModel? Team { get; set; }
 }
